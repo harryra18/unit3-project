a4cbd2d7997a7f4619eca6e6c921fb8c2a5d74db
harryra18 <harryra18@gmail.com>
2025-12-28 19:58:31 -0500

    fix login issue in middleware

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,20 +1,24 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
-  const authHeader = req.headers.authorization
+  const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ message: 'No token provided' })
+    return res.status(401).json({ message: "No token provided" });
   }
 
-  const token = authHeader.split(' ')[1]
+  const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.userId = decoded.id
-    req.user = await User.findById(decoded.id);
-    next()
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.id;
+    req.user = await User.findById(decoded.id).select("_id email");
+
+    next();
   } catch (err) {
-    res.status(401).json({ message: 'Invalid token' })
+    console.error("Verify token error:", err);
+    res.status(401).json({ message: "Invalid token" });
   }
-}
+};
