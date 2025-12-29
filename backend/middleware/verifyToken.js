4e5c44b7489ed19af32e21aaaa02dfe2d9eee89c
harryra18 <harryra18@gmail.com>
2025-12-28 19:42:37 -0500

    fix login issue?

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -12,6 +12,7 @@ module.exports = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     req.userId = decoded.id
+    req.user = await User.findById(decoded.id);
     next()
   } catch (err) {
     res.status(401).json({ message: 'Invalid token' })
