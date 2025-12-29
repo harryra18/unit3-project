91b3c015db907249e3e02ec3a33724c8df394f12
harryra18 <harryra18@gmail.com>
2025-12-28 19:48:34 -0500

    async function

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization
 
   if (!authHeader) {
